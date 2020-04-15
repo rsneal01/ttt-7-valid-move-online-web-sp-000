5a99c0363b954dc81ed5024a12ec743ca337bab2
Bobby Neal <rsneal01@gmail.com>
2020-04-15 15:56:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,17 @@
 def valid_move?(board, index)
+<<<<<<< HEAD
   if index.between?(0, 8) && !position_taken?(board, index)
+=======
+  if board[index].between?(0, 8)
+>>>>>>> fb17f56f7ae6e1b5f2aaec9662282fe7d3e32986
     return true
   else
     return false
   end
+<<<<<<< HEAD
+=======
+  position_taken?(board, index)
+>>>>>>> fb17f56f7ae6e1b5f2aaec9662282fe7d3e32986
 end
 
 
